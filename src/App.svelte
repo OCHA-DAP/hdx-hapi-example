62eb93e1a834e446f849dedc293f8c9fcc21e46e
baripembo <erikawei@gmail.com>
2023-09-28 16:42:58 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Map from './lib/Map.svelte'
+</script>
+
+<main>
+  <h1>Title</h1>
+  
+  <h2 class='header'>Header</h2>
+  <div class='grid-container'>
+    <div class='key-figure col-2'>1</div>
+    <div class='key-figure col-2'>2</div>
+    <div class='key-figure col-2'>3</div>
+    <div class='key-figure col-2'>4</div>
+    <div class='key-figure col-2'>5</div>
+    <div class='key-figure col-2'>6</div>
+  </div>
+
+  <h2 class='header'>Header</h2>
+
+  <div class='tabs'>
+    <div class='tab active'><a href='#' title='People in Need'>People in Need</a></div>
+    <div class='tab'><a href='#' title='Internally Displaced People'>Internally Displaced People</a></div>
+    <div class='tab'><a href='#' title='Refugees'>Refugees</a></div>
+    <div class='tab'><a href='#' title='Returnees'>Returnees</a></div>
+    <div class='tab'><a href='#' title='Organizations'>Organizations</a></div>
+    <div class='tab'><a href='#' title='Food Insecurity'>Food Insecurity</a></div>
+    <div class='tab'><a href='#' title='Malnutrition'>Malnutrition</a></div>
+    <div class='tab'><a href='#' title='Health'>Health</a></div>
+  </div>
+
+  <div class='content grid-container'>
+    <div class='sidebar col-2'>
+      sidebar
+    </div>
+    <div class='main-content col-10'>
+      <Map />
+    </div>
+  </div>
+</main>
+
+<style lang='scss'>
+  .main-content  {
+    padding: 0;
+  }
+</style>
---
+++ src/lib/Map.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { Map } from 'mapbox-gl';
+	import { onMount, onDestroy } from 'svelte';
+	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css'
+
+	let map;
+	let mapContainer;
+
+	// mapboxgl.baseApiUrl='https://data.humdata.org/mapbox';
+	// mapboxgl.accessToken = 'cacheToken';
+
+	onMount(() => {
+		//calculate available space for map
+		let mapHeight = window.innerHeight - document.getElementsByClassName('map')[0].getBoundingClientRect().top - 30;
+		document.getElementsByClassName('map-container')[0].style.height = mapHeight + 'px';
+
+	  map = new Map({
+	    container: mapContainer,
+	    accessToken: 'pk.eyJ1IjoiaHVtZGF0YSIsImEiOiJja2FvMW1wbDIwMzE2MnFwMW9teHQxOXhpIn0.Uri8IURftz3Jv5It51ISAA',
+	    style: `mapbox://styles/humdata/cl3lpk27k001k15msafr9714b`,
+	    center: [18, 15],
+	    zoom: 4.8
+	  });
+	});
+
+	onDestroy(() => {
+	  map.remove();
+	});
+</script>
+
+<div class='map-container'>
+  <div class='map' bind:this={mapContainer} />
+</div>
+
+<style lang='scss'>
+	.map {
+  	height: 100%;
+	}
+</style>
