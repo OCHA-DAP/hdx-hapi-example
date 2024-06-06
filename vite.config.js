b894713a14d4e77c1c5667672a1e8a0594a7e312
baripembo <erikawei@gmail.com>
2024-06-05 23:04:07 -0400

    update base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/viz-datagrid-dashboard/',
+  base: '/hdx-hapi-example/',
   plugins: [
     svelte()
   ],
